e0471b5d09ee5e26a824b60434fe92c9582efdd0
Pavith Priyadharsan S <146210938+PavithPriyadharsan@users.noreply.github.com>
2024-04-12 20:51:29 +0530

    Update vercel.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
-const path = require('path')
-const webpack = require('webpack')
-
-module.exports = {
-  entry: './frontend-js/main.js',
-  output: {
-    filename: 'main-bundled.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
+{
+  "builds": [
+    { "src": "frontend-js/main.js", "use": "@vercel/static-build" }
+  ],
+  "routes": [
+    { "handle": "filesystem" },
+    { "src": "/(.*)", "dest": "frontend-js/main.js" }
+  ]
 }
